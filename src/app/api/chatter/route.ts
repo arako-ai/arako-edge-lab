54bfcad7f26900a047710b33eb7aa2362acd5d6a
Natthapach <natthapach.an@chomchob.com>
2025-02-05 16:15:45 +0700

    src: add log body

--- src/app/api/chatter/route.ts
+++ src/app/api/chatter/route.ts
@@ -7,5 +7,6 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   console.log("request", request);
+  console.log("request.body", request.body);
   return NextResponse.json({ message: "Hi, POST Chatter!", text: "Hello" });
 }
