962bcffa98ccc9d22a527e8cbb3c01ef342b5419
Natthapach <natthapach.an@chomchob.com>
2025-02-05 16:11:18 +0700

    src: add text res

--- src/app/api/chatter/route.ts
+++ src/app/api/chatter/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   console.log("request", request);
-  return NextResponse.json({ message: "Hi, GET Chatter!" });
+  return NextResponse.json({ message: "Hi, GET Chatter!", text: "Hello" });
 }
 
 export async function POST(request: Request) {
   console.log("request", request);
-  return NextResponse.json({ message: "Hi, POST Chatter!" });
+  return NextResponse.json({ message: "Hi, POST Chatter!", text: "Hello" });
 }
